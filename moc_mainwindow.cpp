aaafff7c1dfa4abb48a5deb657ce207c4f4226c8
sasimohammed <144622733+sasimohammed@users.noreply.github.com>
2024-04-24 07:48:58 +0700

    Add files via upload

---
+++ moc_mainwindow.cpp
@@ -0,0 +1,311 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.7.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../mainwindow.h"
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.7.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "applyBrightness",
+    "",
+    "value",
+    "applydarkness",
+    "applyblurs",
+    "on_bright_clicked",
+    "on_purprle_clicked",
+    "on_brows_clicked",
+    "on_dark_clicked",
+    "on_blackwight_clicked",
+    "on_inverter_clicked",
+    "on_gray_clicked",
+    "on_infrafred_clicked",
+    "on_oldtv_clicked",
+    "on_flip_clicked",
+    "on_flipv_clicked",
+    "on_sunilght_clicked",
+    "on_rotation_clicked",
+    "on_eight_clicked",
+    "on_ninie_clicked",
+    "on_two_clicked",
+    "on_frame_clicked",
+    "on_fancy_clicked",
+    "on_normal_clicked",
+    "on_blur_clicked",
+    "on_resize_clicked",
+    "on_merg_clicked",
+    "on_loadnew_clicked",
+    "on_applymerg_clicked",
+    "on_crop_clicked",
+    "on_point_clicked",
+    "on_image_linkActivated",
+    "link",
+    "on_detect_clicked"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      31,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,  200,    2, 0x08,    1 /* Private */,
+       4,    1,  203,    2, 0x08,    3 /* Private */,
+       5,    1,  206,    2, 0x08,    5 /* Private */,
+       6,    0,  209,    2, 0x08,    7 /* Private */,
+       7,    0,  210,    2, 0x08,    8 /* Private */,
+       8,    0,  211,    2, 0x08,    9 /* Private */,
+       9,    0,  212,    2, 0x08,   10 /* Private */,
+      10,    0,  213,    2, 0x08,   11 /* Private */,
+      11,    0,  214,    2, 0x08,   12 /* Private */,
+      12,    0,  215,    2, 0x08,   13 /* Private */,
+      13,    0,  216,    2, 0x08,   14 /* Private */,
+      14,    0,  217,    2, 0x08,   15 /* Private */,
+      15,    0,  218,    2, 0x08,   16 /* Private */,
+      16,    0,  219,    2, 0x08,   17 /* Private */,
+      17,    0,  220,    2, 0x08,   18 /* Private */,
+      18,    0,  221,    2, 0x08,   19 /* Private */,
+      19,    0,  222,    2, 0x08,   20 /* Private */,
+      20,    0,  223,    2, 0x08,   21 /* Private */,
+      21,    0,  224,    2, 0x08,   22 /* Private */,
+      22,    0,  225,    2, 0x08,   23 /* Private */,
+      23,    0,  226,    2, 0x08,   24 /* Private */,
+      24,    0,  227,    2, 0x08,   25 /* Private */,
+      25,    0,  228,    2, 0x08,   26 /* Private */,
+      26,    0,  229,    2, 0x08,   27 /* Private */,
+      27,    0,  230,    2, 0x08,   28 /* Private */,
+      28,    0,  231,    2, 0x08,   29 /* Private */,
+      29,    0,  232,    2, 0x08,   30 /* Private */,
+      30,    0,  233,    2, 0x08,   31 /* Private */,
+      31,    0,  234,    2, 0x08,   32 /* Private */,
+      32,    1,  235,    2, 0x08,   33 /* Private */,
+      34,    0,  238,    2, 0x08,   35 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   33,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'applyBrightness'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'applydarkness'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'applyblurs'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_bright_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_purprle_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_brows_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_dark_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_blackwight_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_inverter_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_gray_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_infrafred_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_oldtv_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_flip_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_flipv_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_sunilght_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_rotation_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_eight_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_ninie_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_two_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_frame_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_fancy_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_normal_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_blur_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_resize_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_merg_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_loadnew_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_applymerg_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_crop_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_point_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_image_linkActivated'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        // method 'on_detect_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->applyBrightness((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 1: _t->applydarkness((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 2: _t->applyblurs((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 3: _t->on_bright_clicked(); break;
+        case 4: _t->on_purprle_clicked(); break;
+        case 5: _t->on_brows_clicked(); break;
+        case 6: _t->on_dark_clicked(); break;
+        case 7: _t->on_blackwight_clicked(); break;
+        case 8: _t->on_inverter_clicked(); break;
+        case 9: _t->on_gray_clicked(); break;
+        case 10: _t->on_infrafred_clicked(); break;
+        case 11: _t->on_oldtv_clicked(); break;
+        case 12: _t->on_flip_clicked(); break;
+        case 13: _t->on_flipv_clicked(); break;
+        case 14: _t->on_sunilght_clicked(); break;
+        case 15: _t->on_rotation_clicked(); break;
+        case 16: _t->on_eight_clicked(); break;
+        case 17: _t->on_ninie_clicked(); break;
+        case 18: _t->on_two_clicked(); break;
+        case 19: _t->on_frame_clicked(); break;
+        case 20: _t->on_fancy_clicked(); break;
+        case 21: _t->on_normal_clicked(); break;
+        case 22: _t->on_blur_clicked(); break;
+        case 23: _t->on_resize_clicked(); break;
+        case 24: _t->on_merg_clicked(); break;
+        case 25: _t->on_loadnew_clicked(); break;
+        case 26: _t->on_applymerg_clicked(); break;
+        case 27: _t->on_crop_clicked(); break;
+        case 28: _t->on_point_clicked(); break;
+        case 29: _t->on_image_linkActivated((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 30: _t->on_detect_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 31)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 31;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 31)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 31;
+    }
+    return _id;
+}
+QT_WARNING_POP
